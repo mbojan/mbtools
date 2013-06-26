743d2898330ae0732fab6b627051f002748f3eae
Michal Bojanowski <michal2992@gmail.com>
2013-06-26 15:05:48 +0200

    Updated package documentation

---
+++ R/mbtools-package.R
@@ -0,0 +1,13 @@
+#' Collection of development tools for R
+#' 
+#' This package is a collection of utility function for working with and
+#' developing for .
+#' 
+#' 
+#' @name mbtools-package
+#' @aliases mbtools-package mbtools
+#' @docType package
+#' @author Michal Bojanowski\cr \email{michal2992@@gmail.com}\cr
+#' \url{http://www.bojanorama.pl}
+#' @keywords package
+NULL
