b8afb93ae05784d2236596294f171b2a93ca988c
Michal Bojanowski <michal2992@gmail.com>
2021-01-19 21:21:09 +0100

    Improve documentation of nullModel()

--- R/nullModel.R
+++ R/nullModel.R
@@ -3,11 +3,18 @@
 #' Fit a null two-level random intercept model using closed-form formulas
 #' provided by Snijders nd Bosker (1999)
 #' 
-#' The function fits a random effect model of the form:
+#' @param y numeric, dependent variable
+#' @param g numeric, grouping factor
+#' @param data data frame, optional dataset where the variables should be
+#' looked for
+#' @param x object of class "nullModel"
+#' @param ... other arguments passed to/from other methods
+#' 
+#' @details The function fits a random effect model of the form:
 #'
 #' \deqn{Y_{ij} = \mu + U_j + R_{ij}}{Y_ij = mu + U_j + R_ij}
 #'
-#' where \eqn{Y_{ij}}{Y_ij} is the value of the dependent variable (\code{y})
+#' where \eqn{Y_{ij}}{Y_ij} is the value of the dependent variable (`y`)
 #' for i-th object within j-th group, \eqn{\mu}{mu} is the population mean,
 #' \eqn{U_j} is the effect specific to group j, and \eqn{R_{ij}}{R_ij} is the
 #' within-grup error.
@@ -15,15 +22,8 @@
 #' The function estimates between-group and pooled within-group variances as
 #' well as the intra-class correlation coefficient.  The closed-form
 #' expressions are provided by Snijders and Bosker (1999, p. 18--21).
-#' 
-#' @param y numeric, dependent variable
-#' @param g numeric, grouping factor
-#' @param data data frame, optional dataset where the variables should be
-#' looked for
-#' @param x object of class "nullModel"
-#' @param ... other arguments passed to/from other methods
 #'
-#' @return The function returns a list of S3 class \code{nullModel} which
+#' @return The function returns a list of S3 class "nullModel" which
 #' contains the following components:
 #' \describe{
 #' \item{yname}{character, name of the dependent variable}
@@ -36,8 +36,8 @@
 #' \item{intcor}{numeric, intra-class correlation coefficient}
 #' }
 #'
-#' @seealso \code{\link[lme4]{lmer}} in package \pkg{lme4} for general fitting
-#' of mixed-effects models.
+#' @seealso `lmer` in package \pkg{lme4} for general fitting of mixed-effects
+#'   models.
 #'
 #' @references Snijders, T.A.B., Bosker, R. (1999) "Multilevel Analysis. An
 #' Introduction to Basic and Advanced Multilevel Modelling". London: Sage
