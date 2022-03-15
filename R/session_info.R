1d00387bbf0fefdd04a1f1c4fa2ac05617b2d388
Michal Bojanowski <michal2992@gmail.com>
2022-03-15 14:37:22 +0100

    Add session_info()

---
+++ R/session_info.R
@@ -0,0 +1,23 @@
+#' Session info for GitHub
+#'
+#' Run `sessioninfo::session_info()`, format the output and copy to clipboard
+#' for easy pasting on GitHub.
+#' 
+#' @export
+
+session_info <- function() {
+  requireNamespace("sessioninfo")
+  requireNamespace("clipboard")
+  z <- capture.output(sessioninfo::session_info())
+  ch <- paste(
+    "<details>",
+    "<summary>Session info</summary>",
+    "",
+    paste(paste0(paste0(rep(" ", 6), collapse=""), z), collapse="\n"),
+    "",
+    "</details>",
+    sep = "\n"
+  )
+  clipboard::write_cb(ch)
+  
+}
